3ed8a9ce3d16123774517fadbf2a824ca973fe23
Debdut Saha <debdut.saha.1@gmail.com>
2025-12-17 11:51:15 +0530

    Initial Commit

---
+++ fandomsocial/fandomsocialApp.swift
@@ -0,0 +1,17 @@
+//
+//  fandomsocialApp.swift
+//  fandomsocial
+//
+//  Created by Debdut Saha on 17/12/25.
+//
+
+import SwiftUI
+
+@main
+struct fandomsocialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
