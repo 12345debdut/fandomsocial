d08796ebc483069c84f3f90a667a0e2ed0206b8e
Debdut Saha <debdut.saha.1@gmail.com>
2025-12-29 14:01:38 +0530

    Comment section transition and reusable modal added.

---
+++ IOS/fandomsocial/comments/CommentSectionView.swift
@@ -0,0 +1,18 @@
+//
+//  CommentSectionView.swift
+//  fandomsocial
+//
+//  Created by Debdut Saha on 29/12/25.
+//
+
+import SwiftUI
+
+struct CommentSectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CommentSectionView()
+}
---
+++ IOS/fandomsocial/components/CrossModalView.swift
@@ -0,0 +1,18 @@
+//
+//  CrossModalView.swift
+//  fandomsocial
+//
+//  Created by Debdut Saha on 29/12/25.
+//
+
+import SwiftUI
+
+struct CrossModalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CrossModalView()
+}
